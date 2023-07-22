765470ffcf43a9b25a1817a617f26a02deb37d4c
admin <Ivan-nikolaenko@bk.ru>
2023-07-22 21:20:18 +0600

    Initial commit

---
+++ Section_2/Input.py
@@ -0,0 +1,23 @@
+# a = float(input("Введите первый аргумент: "))
+# b = float(input("Введите второй аргумент: "))
+# print("Периметр равен : ", 2 * (a + b))
+# a = int(input())
+# b = int(input())
+# c = int(input())
+a = 7
+b = -4
+c = 3
+print(f"{a}\n{b}\n{c}")
+s1 = "Hello"
+s2 = "Balakirev"
+print(s1, end=" ")
+print(s2)
+# #Word 1: I | Word 2: love
+# s1 = input()
+# s2 = input()
+# print(f"Word 1: {s1} | Word 2: {s2}")
+# a1, a2 = map(float, input().split())
+# a3 = a1+a2
+# print(a3)
+x = float(input())
+print(f"Вы ввели число {x}")
---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello", "Rr", "fuUCK", sep=" | ")
+print("Hello", end=" ")
+
+x = 3
+y = 14
+#print("Координаты точки : x = ", x/2, "; y = ", y)
+print(f"Hello Координаты точки : x = {x} ; y = {y}")
