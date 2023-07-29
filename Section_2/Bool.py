5cb108422bdc3830a8514c84e143730a059e2f64
admin <Ivan-nikolaenko@bk.ru>
2023-07-29 22:17:23 +0600

    Разбор по курсу строки и операции над ними

--- Section_2/Bool.py
+++ Section_2/Bool.py
@@ -1,11 +1,6 @@
-
-
 # a = float(input())
 # b=int(a)
 # print(bool(b % 3 == 0))
 
-#round d = round (3.3433 , 2 ) те ввыводим 2 знака после запятой
+# round d = round (3.3433 , 2 ) те ввыводим 2 знака после запятой
 
-# a,b=map(int,input().split())
-# result =bool(a % b ==0)
-# print(result)
