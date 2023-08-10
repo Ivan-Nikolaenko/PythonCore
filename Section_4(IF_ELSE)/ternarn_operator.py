ab1a33fb9c19745982e8b10d565766460c579fe2
admin <Ivan-nikolaenko@bk.ru>
2023-08-10 22:08:17 +0600

    added lesson on ternary operator

--- Section_4(IF_ELSE)/ternarn_operator.py
+++ Section_4(IF_ELSE)/ternarn_operator.py
@@ -3,8 +3,26 @@
 # res = a if a > b else b
 # print(res)
 
-# s = input()
-# t = input()
-# res = s.upper() if len(s) >= 4 else t
-# print(res)
+# s = float(input())
+# t = float(input())
+# d = s if s > t else t
+# print(d)
 
+# a = int(input())
+# msg = 'кратно 3' if a % 3 == 0 else 'не кратно 3'
+# print(msg)
+
+# str = input()
+# a = str.lower()
+# msg = 'палиндром' if a[:] == a[::-1] else 'не палиндром'
+# print(msg)
+
+# num = int(input())
+# if num == 1:
+#     print('True')
+# else:
+#     print('False')
+
+time = int(input())
+fact = 0 if time+1==59 else time+1
+print(fact)
