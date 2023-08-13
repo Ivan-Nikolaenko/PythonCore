921b8e40ae4f7c98dd142a8d024bb75b3739b9a1
admin <Ivan-nikolaenko@bk.ru>
2023-08-13 22:07:58 +0600

    Присутпил к циклам

---
+++ Section_5(WhileFor)/lesson_1_while.py
@@ -0,0 +1,40 @@
+# i = 1
+# var = 0
+# while i < 10:
+#     i+=1
+#     print(i)
+
+# pass_word = 'pass'
+# ps = ''
+#
+# while ps != pass_word:
+#     ps = input("Введите пароль: ")
+#
+# print("Вход в систему ")
+
+# n, m = map(int, input().split())
+# while n <= m:  # Запускаем цикл while с условием n <= m
+#     print(n ** 2, end=' ')  # Выводим квадрат значения n
+#     n += 1  # Увеличиваем значение n на 1
+
+# price = float(input())
+# count = 2
+# while count <= 10:
+#     cost = round(price * count,1)
+#     print(cost, end=' ')  # Выводим стоимость
+#     count += 1  # Увеличиваем счетчик на 1
+
+# a = int(input())
+# stop = 0
+# summ = 0
+# while a != stop:
+#     summ += a
+#     a = int(input())
+# print(summ)
+#
+# s = input()
+#
+# while "--" in s:  # Пока в строке есть двойные дефисы
+#     s = s.replace("--", "-")  # Заменяем двойные дефисы на одинарный дефис
+#
+# print(s)  # Выводим преобразованную строку на экран
