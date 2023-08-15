d2bb7bf7574da3509158c950582e6f0d272d3338
admin <Ivan-nikolaenko@bk.ru>
2023-08-15 21:12:41 +0600

    Приступил к циклу for

---
+++ Section_5(WhileFor)/lesson_3_for.py
@@ -0,0 +1,22 @@
+# d = [1, 3, 4, 5, 10, 13, 24, 54, False]
+#
+# for i in 'python':
+#     print(i)
+# result = ""
+# for i in range(0, 11):
+#     result+=str(i)
+# print(result)
+# result = ""
+# for i in range(10, -1, -1):
+#     result += str(i)+' '
+# print(result)
+
+# result = ""
+# for i in range(-10, -1, 2):
+#     result += str(i)+' '
+# print(result)
+
+result = ""
+for i in range(1, 20, 3):
+    result += str(i)+' '
+print(result)
